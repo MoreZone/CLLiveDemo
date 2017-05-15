8ac16179a1d12c23f9bdd45fe81e85edee465477
MoreZone <conglin1991@sina.com>
2017-05-15 10:29:53 +0800

    初始化

---
+++ LiveStudy/LiveStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStudy
+//
+//  Created by More on 17/5/15.
+//  Copyright © 2017年 More. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStudy/LiveStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStudy
+//
+//  Created by More on 17/5/15.
+//  Copyright © 2017年 More. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
